76fcbc7fb818962c3a967758b8f4ef18a311d715
Elsic <ovstokke@gmail.com>
2016-06-10 14:14:20 +0200

    Changed logic

--- Workout/RandomWod.ps1
+++ Workout/RandomWod.ps1
@@ -1,17 +1,18 @@
 #$path = Read-Host "file location"
+#$wodlist = Get-Content "C:\GitHub\Div\Workout\WODs.txt"
 
-$wodlist = Get-Content "C:\GitHub\Div\Workout\WODs.txt"
+$url = "https://raw.githubusercontent.com/Elsic/Div/master/Workout/WODs.txt"
+$output = "$PSScriptRoot\DownloadedWODs.txt"
+Invoke-WebRequest -Uri $url -OutFile $output
+$wodlist = Get-Content $output
+
 $z = "3"
 
-#$wods = Get-Content $path
-
 $i = $wodlist.Length
-
 
 for ($y=1; $y -le $z; $y++) {
     $wodnr = Get-Random -Minimum 0 -Maximum $i
     $wodlist[$wodnr]
 }
 
-
-#for($i=1; $i -le 10; $i++){Write-Host $i}
+Remove-Item $output -Force
