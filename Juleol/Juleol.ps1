367413e5e7e7122315d17b09e8b248698e2892af
Elsic <ovstokke@gmail.com>
2016-12-22 13:01:40 +0100

    Juleøl test

---
+++ Juleol/Juleol.ps1
@@ -0,0 +1,29 @@
+﻿#juleøl
+
+
+$url = "http://www.adressa.no/masq/juleoltest/2016/data/butikk.json"
+$output = "$PSScriptRoot\butikk.json"
+
+Invoke-WebRequest -Uri $url -OutFile $output
+
+
+$url2 = "http://www.adressa.no/masq/juleoltest/2016/data/sterk.json"
+$output2 = "$PSScriptRoot\sterk.json"
+
+Invoke-WebRequest -Uri $url2 -OutFile $output2
+
+#Get-Content $output -Encoding UTF8
+
+
+
+$json = Get-Content -Path $output -Encoding UTF8 | ConvertFrom-Json
+
+
+$json2 = Get-Content -Path $output2 -Encoding UTF8| ConvertFrom-Json
+
+
+$json
+$json2
+
+$beer = $json + $json2
+$beer
